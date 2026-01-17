1474c01ca92d62fd3a26b10a1a436f7ea342a89e
Isaac <isaacekaro864@gmail.com>
2026-01-17 23:59:00 +0100

    installed appwrite api for authentication for the login and register and created a themedtextinput for texts incase of the change of themes and updated the package json's..

--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -1,23 +1,45 @@
-import { StyleSheet, Text } from 'react-native'
+import { StyleSheet, Text, TouchableWithoutFeedback, Keyboard } from 'react-native'
 import { Link } from 'expo-router'
 
 import ThemedView from '../../components/ThemedView'
 import ThemedText from '../../components/ThemedText'
 import Spacer from '../../components/Spacer'
 import ThemedButton from '../../components/ThemedButton'
+import ThemedTextInput from '../../components/ThemedTextInput'
+import { useState } from 'react'
 
 const Register= () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     const handleSubmit = () => {
-        console.log('register form submitted')
+        console.log('register form submitted', email, password)
     }
 
   return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
     <ThemedView style={styles.container}>
         <Spacer/>
         <ThemedText title={true} style={styles.title}>
         Register your account
         </ThemedText>
 
+        <ThemedTextInput
+          style={{ width: '80%', marginBottom: 20 }}
+            placeholder="Email"
+            keyboardType="email-address"
+           onChangeText={setEmail}
+           value={email}
+         />
+
+         <ThemedTextInput
+          style={{ width: '80%', marginBottom: 20 }}
+            placeholder="Password"
+            keyboardType="default"
+           onChangeText={setPassword}
+           value={password}
+           secureTextEntry
+         />
+        
         <ThemedButton onPress={handleSubmit}>
             <Text style={{ color: '#f2f2f2' }}>Register</Text>
         </ThemedButton>
@@ -29,6 +51,7 @@ const Register= () => {
             </ThemedText>
         </Link>
     </ThemedView>
+    </TouchableWithoutFeedback>
   )
 }
 
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,24 +1,25 @@
+import 'react-native-url-polyfill/auto'
 import { StyleSheet, useColorScheme } from 'react-native'
 import { Stack } from 'expo-router'
 import { Colors } from '../constants/Colors'
 import { StatusBar } from 'expo-status-bar'
 
 const RootLayout = () => {
-    const colorScheme = useColorScheme()
-    const theme = Colors[colorScheme] ?? Colors.light
+  const colorScheme = useColorScheme()
+  const theme = Colors[colorScheme] ?? Colors.light
 
   return (
     <>
-        <StatusBar value="auto" />
-        <Stack screenOptions={{
-            headerStyle: { backgroundColor: theme.navBackground },
-            headerTintColor: theme.title,
-        }}>
+      <StatusBar value="auto" />
+      <Stack screenOptions={{
+        headerStyle: { backgroundColor: theme.navBackground },
+        headerTintColor: theme.title,
+      }}>
 
-            <Stack.Screen name="(auth)" options={{headerShown: false}} />
-            <Stack.Screen name="(dashboard)" options={{headerShown: false}} />
-            <Stack.Screen name="index" options={{ title: 'Home'}}/>
-        </Stack>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ title: 'Home' }} />
+      </Stack>
     </>
   )
 }

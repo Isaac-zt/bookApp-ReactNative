0903a4dea6656f2266e8ea21ca6f25f616ed0f13
Isaac <isaacekaro864@gmail.com>
2026-01-17 20:16:32 +0100

    made some changes t the login.jsx and register.jsx by adding a button to them and now create a new folder for the books and profile page and create

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,22 +1,34 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Pressable, Text } from 'react-native'
 import { Link } from 'expo-router'
+import { Colors } from '../../constants/Colors'
 
 import ThemedView from '../../components/ThemedView'
 import ThemedText from '../../components/ThemedText'
 import Spacer from '../../components/Spacer'
+import ThemedButton from '../../components/ThemedButton'
 
 const Login = () => {
+
+    const handleSubmit = () => {
+        console.log('login form submitted')
+    }
+
   return (
     <ThemedView style={styles.container}>
+
         <Spacer/>
         <ThemedText title={true} style={styles.title}>
         Login to your account
         </ThemedText>
 
+        <ThemedButton onPress={handleSubmit}>
+            <Text style={{ color: '#f2f2f2' }}>Login</Text>
+        </ThemedButton>
+
         <Spacer height={100} />
         <Link href='/register'>
             <ThemedText style={{ textAlign: 'center' }}>
-                Register
+                Register Instead
             </ThemedText>
         </Link>
     </ThemedView>
@@ -29,10 +41,19 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
+        alignItems: 'center',
     },
     title: {
         textAlign: 'center',
         fontSize: 18,
         marginBottom: 30
+    },
+    btn: {
+        backgroundColor: Colors.primary,
+        padding: 15,
+        borderRadius: 5,
+    },  
+    pressed: {
+        opacity: 0.8,
     },
 })
